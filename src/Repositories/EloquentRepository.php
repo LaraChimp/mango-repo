902690988cc96946df6d4d2b1062ddec8f759f38
Percy Mamedy <percymamedy@gmail.com>
2017-09-24 16:48:22 +0000

    Apply fixes from StyleCI

--- src/Repositories/EloquentRepository.php
+++ src/Repositories/EloquentRepository.php
@@ -5,7 +5,7 @@ namespace LaraChimp\MangoRepo\Repositories;
 use LaraChimp\MangoRepo\Concerns;
 use LaraChimp\MangoRepo\Contracts\Repository;
 
-abstract class  EloquentRepository implements Repository
+abstract class EloquentRepository implements Repository
 {
     use Concerns\IsRepositorable,
         Concerns\IsRepositoryBootable,
